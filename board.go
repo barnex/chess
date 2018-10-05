9adff037f2833dc8ab7df29a45943ef2cff615b3
Arne Vansteenkiste <barnex@gmail.com>
2018-10-05 17:00:35 +0100

    board test

--- board.go
+++ board.go
@@ -22,9 +22,12 @@ func NewBoard() *Board {
 
 func (b *Board) String() string {
 	var buf bytes.Buffer
-	for r := 7; r >= 0; r-- {
-		for c := 0; c < 7; c++ {
-			fmt.Fprint(&buf, b.At(RC(r, c)), " ")
+	for r := 0; r < 8; r++ {
+		for c := 0; c < 8; c++ {
+			fmt.Fprint(&buf, b.At(RC(r, c)))
+			if c < 7 {
+				fmt.Fprint(&buf, " ")
+			}
 		}
 		fmt.Fprintln(&buf)
 	}
