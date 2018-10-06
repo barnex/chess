48acc38ceda0866810d98fd72100b01bb4042e6c
Arne Vansteenkiste <barnex@gmail.com>
2018-10-06 09:10:30 +0100

    black pawn

--- rules.go
+++ rules.go
@@ -6,6 +6,8 @@ func Moves(b *Board, src Pos, dst *[]Pos) {
 		return
 	case wP:
 		WPMoves(b, src, dst)
+	case bP:
+		BPMoves(b, src, dst)
 	}
 }
 
@@ -27,6 +29,28 @@ func WPMoves(b *Board, src Pos, dst *[]Pos) {
 	}
 
 	if p := src.Add(FrontRight); p.Valid() && b.At(p).Color() == -me.Color() {
+		*dst = append(*dst, p)
+	}
+}
+
+func BPMoves(b *Board, src Pos, dst *[]Pos) {
+	me := b.At(src)
+
+	// one row forward
+	if p := src.Add(Back); p.Valid() && b.At(p) == 00 {
+		*dst = append(*dst, p)
+	}
+
+	// two rows forward
+	if p := src.Add(Pos{-2, 0}); p.Row() == 4 && b.At(p) == 00 && b.At(src.Add(Back)) == 00 {
+		*dst = append(*dst, p)
+	}
+
+	if p := src.Add(BackLeft); p.Valid() && b.At(p).Color() == -me.Color() {
+		*dst = append(*dst, p)
+	}
+
+	if p := src.Add(BackRight); p.Valid() && b.At(p).Color() == -me.Color() {
 		*dst = append(*dst, p)
 	}
 }
