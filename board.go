fdfbaef743b6321a0a226161f38d6102943a08e0
Arne Vansteenkiste <barnex@gmail.com>
2018-10-11 17:26:21 +0100

    render a board

--- board.go
+++ board.go
@@ -31,6 +31,17 @@ func Upright(b *Board) *Board {
 	return b2
 }
 
+func (b *Board) Slice() [][]Piece {
+	s := make([][]Piece, 8)
+	for i := range s {
+		s[i] = make([]Piece, 8)
+		for j := range s[i] {
+			s[i][j] = b.At(RC(i, j))
+		}
+	}
+	return s
+}
+
 func (b *Board) At(p Pos) Piece {
 	return b[p.Index()]
 }
--- cmd/chess-server/main.go
+++ cmd/chess-server/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/barnex/chess"
 )
 
 var flagAddr = flag.String("http", "localhost:26355", "HTTP address")
@@ -16,5 +19,71 @@ func main() {
 }
 
 func handlePlay(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "chess?")
+
+	d := &data{
+		I:     []int{7, 6, 5, 4, 3, 2, 1, 0},
+		J:     []int{0, 1, 2, 3, 4, 5, 6, 7},
+		Board: chess.NewBoard(),
+	}
+
+	err := templ.Execute(w, d)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
+
+type data struct {
+	I     []int
+	J     []int
+	Board *chess.Board
+}
+
+func (d *data) At(i, j int) chess.Piece {
+	return d.Board.At(chess.RC(i, j))
+}
+
+func (d *data) Oddness(i, j int) string {
+	if (i+j)%2 == 0 {
+		return "even"
+	} else {
+		return "odd"
+	}
+}
+
+var templ = template.Must(template.New("play").Parse(templText))
+
+const templText = `
+<html>
+<head>
+	<style>
+		.odd{
+			background-color: #759455;	
+		}	
+		.even{
+			background-color: #efefd3;	
+		}	
+		td{
+			width: 2em;	
+			height: 2em;	
+			font-size: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+
+<table>
+{{range $x,$i := $.I}}
+	<tr>
+	{{range $x,$j := $.J}}
+	<td class="{{$.Oddness $i $j}}"> {{$.At $i $j}} </td>
+	{{end}}
+	</tr>
+{{end}}
+</table>
+
+</body>
+</html>
+`
