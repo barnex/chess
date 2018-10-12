4cd90c21583f813abe09e643786b0dd3c4e79020
Arne Vansteenkiste <barnex@gmail.com>
2018-10-12 21:14:36 +0100

    also render nicely on black terminal

--- cmd/chess-cli/main.go
+++ cmd/chess-cli/main.go
@@ -10,6 +10,8 @@ import (
 )
 
 func main() {
+	fmt.Println()
+
 	b := NewBoard()
 
 	engine := Minimax(4, Heuristic2)
@@ -47,36 +49,36 @@ const (
 	halfL = "\u258C"
 	halfR = "\u2590"
 
-	bgLight = "\033[48;5;256m"
-	bgDark  = "\033[48;5;251m"
-	fgLight = "\033[38;5;256m"
-	fgDark  = "\033[38;5;251m"
+	bgLight = "\033[48;5;255m"
+	bgDark  = "\033[48;5;250m"
+	fgLight = "\033[38;5;255m"
+	fgDark  = "\033[38;5;250m"
 	fgBlack = "\033[38;5;232m"
 
-	reset = "\033[0m"
+	reset = "\033[39;49m"
 )
 
 func Render(b *Board) {
 	for r := 7; r >= 0; r-- {
 
-		fmt.Print(fgDark, r+1, reset)
+		fmt.Print(reset, fgDark, r+1, reset)
 
 		for c := 0; c < 8; c++ {
 
-			if c == 0 {
+			switch c {
+			case 0:
 				if (c+r)%2 == 1 {
-					fmt.Print(reset, bgLight, fgDark, halfR, bgDark, fgBlack)
+					fmt.Print(reset, fgDark, halfR, bgDark, fgBlack)
 				} else {
-					fmt.Print(" ")
+					fmt.Print(reset, fgLight, halfR, bgLight, fgBlack)
 				}
-			} else {
+			default:
 				if (c+r)%2 == 1 {
 					fmt.Print(reset, bgLight, fgDark, halfR, bgDark, fgBlack)
 				} else {
 					fmt.Print(reset, bgLight, fgDark, halfL, bgLight, fgBlack)
 				}
 			}
-
 			piece := b.At(RC(r, c)).String()
 			if b.At(RC(r, c)) == 0 {
 				piece = " "
@@ -84,14 +86,14 @@ func Render(b *Board) {
 			fmt.Print(piece)
 
 		}
-		if (r)%2 == 1 {
-			fmt.Print(" ")
+		if (r)%2 == 0 {
+			fmt.Print(reset, fgDark, halfL, reset)
 		} else {
-			fmt.Print(reset, bgLight, fgDark, halfL, bgLight, fgBlack)
+			fmt.Print(reset, fgLight, halfL, reset)
 		}
 		fmt.Println()
 	}
-	fmt.Print(fgDark, "  a b c d e f g h", reset, "\n")
+	fmt.Print(reset, fgDark, "  a b c d e f g h", reset, "\n")
 }
 
 func Allowed(b *Board, c Color, m Move) bool {
