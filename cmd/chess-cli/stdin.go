86f8b0784af2672ce8d2bb3b6ebb2162e345d267
Arne Vansteenkiste <barnex@gmail.com>
2018-10-13 11:11:13 +0100

    stdin: parse 3-character moves

--- cmd/chess-cli/stdin.go
+++ cmd/chess-cli/stdin.go
@@ -35,25 +35,64 @@ func Parse(line string, b *Board, c Color) (Move, error) {
 	switch len(line) {
 	default:
 		return Move{}, fmt.Errorf("syntax error: %q", line)
-	case 4:
-		return Parse4(line)
 	case 2:
 		return Parse2(line, b, c)
+	case 3:
+		return Parse3(line, b, c)
+	case 4:
+		return Parse4(line)
 	}
 }
 
 // Parse2 parses a 2-character move, like
 // 	a3
+// which means to move a pawn to a3.
+// Returns an error if the move is ambiguous of not allowed.
 func Parse2(line string, b *Board, c Color) (Move, error) {
-	dst, err := ParsePos(line)
+	return Parse3("P"+line, b, c)
+	//dst, err := ParsePos(line)
+	//if err != nil {
+	//	return Move{}, err
+	//}
+
+	//myPawn := WP * Piece(c)
+	//var cand []Move
+	//for _, a := range AllMoves(b, c) {
+	//	if b.At(a.Src) == myPawn && a.Dst == dst {
+	//		cand = append(cand, a)
+	//	}
+	//}
+	//switch len(cand) {
+	//case 0:
+	//	return Move{}, fmt.Errorf("%v not allowed", line)
+	//case 1:
+	//	return cand[0], nil
+	//default:
+	//	return Move{}, fmt.Errorf("%v is ambigous: %v match", line, cand)
+	//}
+
+}
+
+// Parse3 parses a 3-character move, like
+// 	Nf3
+// which means to move a knight to f3.
+// Returns an error if the move is ambiguous of not allowed.
+func Parse3(line string, b *Board, c Color) (Move, error) {
+
+	p, err := ParsePiece(line[:1])
 	if err != nil {
 		return Move{}, err
 	}
 
-	myPawn := WP * Piece(c)
+	dst, err := ParsePos(line[1:])
+	if err != nil {
+		return Move{}, err
+	}
+
+	myPiece := p * Piece(c)
 	var cand []Move
 	for _, a := range AllMoves(b, c) {
-		if b.At(a.Src) == myPawn && a.Dst == dst {
+		if b.At(a.Src) == myPiece && a.Dst == dst {
 			cand = append(cand, a)
 		}
 	}
@@ -66,6 +105,21 @@ func Parse2(line string, b *Board, c Color) (Move, error) {
 		return Move{}, fmt.Errorf("%v is ambigous: %v match", line, cand)
 	}
 
+}
+
+func ParsePiece(line string) (Piece, error) {
+	p, ok := map[string]Piece{
+		"P": WP, "p": WP,
+		"R": WR, "r": WR,
+		"N": WN, "n": WN,
+		"B": WB, "b": WB,
+		"Q": WQ, "q": WQ,
+		"K": WK, "k": WK,
+	}[line]
+	if !ok {
+		return 00, fmt.Errorf("no such piece %q, options: R N B Q K", line)
+	}
+	return p, nil
 }
 
 // Parse4 parses a 4-character move, like
