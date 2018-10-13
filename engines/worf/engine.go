438a1d2bc7f44c5b23b58b69fac161ee592c579d
Arne Vansteenkiste <barnex@gmail.com>
2018-10-13 20:39:39 +0100

    speedup worf 61ms

--- engines/worf/engine.go
+++ engines/worf/engine.go
@@ -17,13 +17,13 @@ type worf struct {
 }
 
 func (e *worf) Move(b *Board, c Color) (Move, float64) {
-
 	var (
 		bestMove  = Move{}
 		bestScore = Inf(-1)
 	)
 	for _, m := range AllMoves(b, c) {
-		s := float64(e.negamax(b, e.depth, c, m)) + e.noise()
+		n := &Node{*b, Heuristic2(b, m)}
+		s := float64(e.negamax(n, e.depth, c, m)) + e.noise()
 		if s > bestScore {
 			bestScore = s
 			bestMove = m
@@ -32,28 +32,54 @@ func (e *worf) Move(b *Board, c Color) (Move, float64) {
 	return bestMove, bestScore
 }
 
-func (e *worf) negamax(b *Board, depth int, c Color, m Move) int {
+type Node struct {
+	board Board
+	value int
+}
 
-	if dst := b.At(m.Dst); dst == WK || dst == BK {
+func (n *Node) WithMove(m Move) *Node {
+	return &Node{
+		*n.board.WithMove(m),
+		//Heuristic3(n, m),
+		Heuristic2(&n.board, m),
+	}
+}
+
+func (e *worf) negamax(n *Node, depth int, c Color, m Move) int {
+
+	if dst := n.board.At(m.Dst); dst == WK || dst == BK {
 		return inf(-c * dst.Color())
 	}
 
 	if depth == 0 {
-		return e.Heuristic2(b, c, m)
+		return int(c) * Heuristic3(n, m)
 	}
 
 	value := inf(1)
 
-	b2 := b.WithMove(m)
-	b = nil
-	for _, m := range AllMoves(b2, -c) {
-		v := e.negamax(b2, depth-1, -c, m) * -1
+	n2 := n.WithMove(m)
+	n = nil
+	for _, m := range AllMoves(&n2.board, -c) {
+		v := e.negamax(n2, depth-1, -c, m) * -1
 		value = min(value, v)
 	}
 	return value
 }
 
-func (e *worf) Heuristic2(b *Board, c Color, m Move) int {
+func Heuristic3(n *Node, m Move) int {
+
+	delta := -valueOf[n.board.At(m.Dst)+6]
+	fast := (n.value + delta)
+
+	//check := Heuristic2(&n.board, m)
+	//fmt.Printf("have %v, delta %v, want %v\n", n.value, delta, check)
+	//if fast != check {
+	//   panic(fmt.Sprintf("%v != %v", fast, check))
+	//}
+	return fast
+}
+
+func Heuristic2(b *Board, m Move) int {
 	NumEvals++
 
 	b = b.WithMove(m)
@@ -61,7 +87,7 @@ func (e *worf) Heuristic2(b *Board, c Color, m Move) int {
 	for _, p := range b {
 		h += valueOf[p+6]
 	}
-	return int(c) * h
+	return h
 }
 
 var valueOf = [13]int{
@@ -70,7 +96,6 @@ var valueOf = [13]int{
 	WB + 6: 5,
 	WR + 6: 10,
 	WQ + 6: 20,
-
 	BP + 6: -1,
 	BN + 6: -6,
 	BB + 6: -5,
