537d2ff4eb3e7fe2900b6d4275a6b400846f9e40
Arne Vansteenkiste <barnex@gmail.com>
2018-10-14 13:23:18 +0100

    stash

--- cmd/chess-cli/render.go
+++ cmd/chess-cli/render.go
@@ -25,26 +25,26 @@ func Render(b *Board) {
 		fmt.Print(reset, fgDark, r+1, reset)
 
 		for c := 0; c < 8; c++ {
-
+			fmt.Print(reset)
 			switch c {
 			case 0:
 				if (c+r)%2 == 0 {
-					fmt.Print(reset, fgDark, halfR, bgDark, fgBlack)
+					fmt.Print(fgDark, halfR, bgDark)
 				} else {
-					fmt.Print(reset, fgLight, halfR, bgLight, fgBlack)
+					fmt.Print(fgLight, halfR, bgLight)
 				}
 			default:
 				if (c+r)%2 == 0 {
-					fmt.Print(reset, bgLight, fgDark, halfR, bgDark, fgBlack)
+					fmt.Print(bgLight, fgDark, halfR, bgDark)
 				} else {
-					fmt.Print(reset, bgLight, fgDark, halfL, bgLight, fgBlack)
+					fmt.Print(bgLight, fgDark, halfL, bgLight)
 				}
 			}
 			piece := b.At(RC(r, c)).String()
 			if b.At(RC(r, c)) == 0 {
 				piece = " "
 			}
-			fmt.Print(piece)
+			fmt.Print(fgBlack, piece)
 
 		}
 		if (r)%2 == 1 {
